c8d8c1e73fd3be820ec957e4c9b87ae249a99e51
jose <josmontenegro2012@gmail.com>
2018-01-26 22:29:58 -0500

    First version of GameManager complete

---
+++ Assets/Playgrounds/Jose_Playground/GameManager.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	private static GameManager _instance;
+
+	public static GameManager instance{
+		get{
+			if (_instance == null) {
+				_instance = FindObjectOfType<GameManager> ();
+				if (_instance == null) {
+					GameObject obj = new GameObject ();
+					_instance = obj.AddComponent<GameManager> ();
+					DontDestroyOnLoad (_instance);
+				}
+			} else {
+				GameManager ob = FindObjectOfType<GameManager> ();
+				if (ob != null && ob != _instance) {
+					Destroy (ob);
+				}
+			}
+			return _instance;
+		}
+	}
+
+	public float timeLimit = 120f;
+
+	private int kills;
+	private int heals;
+	private float morality;
+	private int baseHealth;
+	private bool gameEnded = false;
+	private float timeLeft;
+
+	void Awake(){
+		Reset ();
+	}
+
+	void Start(){
+		StartCoroutine (StartTimer ());
+	}
+
+	IEnumerator StartTimer(){
+		while (timeLeft > 0f && !gameEnded) {
+			timeLeft -= Time.deltaTime;
+			yield return null;
+		}
+
+		if (gameEnded) {
+			LoseGame ();
+		} else {
+			WinGame ();
+		}
+	}	
+
+	void Reset(){
+		kills = 0;
+		heals = 0;
+		morality = 0.5f;
+		timeLeft = timeLimit;
+		baseHealth = 1;
+		gameEnded = false;
+	}
+
+	void ChangeBaseHealth(int h){
+		baseHealth += h;
+		if (baseHealth <= 0) {
+			LoseGame ();
+		}
+	}
+
+	public void EnemyKill(){
+		kills += 1;
+		UpdateMorality ();
+	}
+
+	public void EnemyHeal(){
+		heals += 1;
+		UpdateMorality ();
+	}
+
+	private void UpdateMorality(){
+		morality = (float)heals / (float)(kills + heals);
+	}
+
+	public float GetTimeLeft(){
+		return timeLeft;
+	}
+
+	public string GetTimeText(){
+		int it = Mathf.CeilToInt (timeLeft);
+		int mins = it / 60;
+		int secs = it % 60;
+		string minutesText = (mins < 10 ? "0" : "") + mins + ":";
+		string secondsText = (secs < 10 ? "0" : "") + secs;
+		return minutesText + secondsText;
+	}
+
+	void WinGame(){
+		Debug.Log ("Time over. You won!");
+		Debug.Log ("Final Morality: " + morality);
+	}
+
+	void LoseGame(){
+		Debug.Log ("Base is Destroyed. Game Over");
+	}
+}
---
+++ Assets/Playgrounds/Jose_Playground/GameManagerTester.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManagerTester : MonoBehaviour {
+
+	Text text;
+	public int heals = 0;
+	public int kills = 0;
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text> ();
+		for (int i = 0; i < heals; ++i) {
+			GameManager.instance.EnemyHeal ();
+		}
+
+		for (int i = 0; i < kills; ++i) {
+			GameManager.instance.EnemyKill ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = "Time Left: " + GameManager.instance.GetTimeText ();
+	}
+}
