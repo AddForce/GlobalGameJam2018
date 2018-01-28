44eb9a5d6540ee85aa3ca3b7717e8362a740643e
Hercules Dias Campos <hercules.amphibolite@gmail.com>
2018-01-27 21:34:49 -0500

    newMessage

--- Assets/Scripts/SceneTransitionScripts/ButtonBehavior.cs
+++ Assets/Scripts/SceneTransitionScripts/ButtonBehavior.cs
@@ -1,16 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ButtonBehavior : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField] private int sceneIndex;
+
+    public void DoSceneChange() { SceneManager.LoadScene(sceneIndex); }
 }
